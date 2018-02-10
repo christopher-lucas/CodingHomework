470fc97de13467e27a4f2e9a9e58d758b00cc249
christopher-lucas <christopher.lucas@utexas.edu>
2018-02-10 16:59:22 -0600

    update

--- HW_04/Education.py
+++ HW_04/Education.py
@@ -1,41 +1,31 @@
-
-# coding: utf-8
-
-# In[12]:
-
-
+#Modules
 import os
 import pandas as pd
 import numpy as np
 
-
-# In[13]:
-
-
+#Import csv with school data
 SchoolFile = os.path.join('04 Pandas', 'PyCitySchools', 'raw_data','schools_complete.csv')
 
-
-# In[36]:
-
-
+#Import csv with student data
 StudentFile = os.path.join('04 Pandas', 'PyCitySchools', 'raw_data','students_complete.csv')
 
-
-# In[37]:
-
-
-student_df = pd.read_csv(StudentFile)
+#Convert school csv to dataframe
 school_df = pd.read_csv(SchoolFile)
 
+#Convert student csv to dataframe
+student_df = pd.read_csv(StudentFile)
 
-# In[46]:
+#Rename 'School ID' and 'name' columns in school dataframe
+school_df.rename(columns={'name': 'school_name', 'School ID' : 'school_id'}, inplace=True)
 
+#Rename 'Stucdent ID' and 'school' columns in student dataframe
+student_df.rename(columns={'school': 'school_name', 'Student ID' : 'student_id', 'name' : 'student_name'}, inplace=True)
 
-school_df.rename(columns={'name': 'school_name'}, inplace=True)
+#Merge the two dataframes based on school name to create ed dataframe
+ed_df = pd.merge(school_df, student_df, on='school_name')
 
+#Pivot table
+pd.pivot_table(school_df,index=["school_name"])
 
-# In[48]:
-
-
-student_df.rename(columns={'school': 'school_name'}, inplace=True)
-
+#Export to csv
+ed.df.to_csv('ed.csv')
